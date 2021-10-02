a264d61ba8e6dc540ec7888768821aeadb72e624
SigongJoa <59388484+jhwgud@users.noreply.github.com>
2021-10-03 02:55:09 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
     echo "test"
+    <script>        
+      function openRandomNovel() {
+        let random = Math.floor(Math.random() * (52000 - 1) + 1);
+        window.open(`https://novelpia.com/novel/${random}`, "_blank");
+      }
+    </script>
 
 ?>
